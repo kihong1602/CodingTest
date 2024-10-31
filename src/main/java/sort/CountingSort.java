e67a80c1d600e2a7baade310e2265c9c2f519c49
Kihong <kks4517@naver.com>
2024-10-31 14:21:21 +0900

    ✅ Update CountingSort

---
+++ src/main/java/sort/CountingSort.java
@@ -0,0 +1,22 @@
+package sort;
+
+public class CountingSort {
+
+  public static void sort(int[] array, int range) {
+    int[] count = new int[range + 1];
+
+    for (int value : array) {
+      count[value]++;
+    }
+
+    int idx = 0;
+    for (int i = 1; i < count.length; i++) {
+      int size = count[i];
+      while (size-- > 0) {
+        array[idx++] = i;
+      }
+    }
+
+  }
+  
+}
--- src/test/java/sort/SortTest.java
+++ src/test/java/sort/SortTest.java
@@ -116,6 +116,19 @@ class SortTest {
     loggingExecutionTime(start, end);
   }
 
+  @Test
+  @DisplayName("카운팅 정렬 테스트")
+  void countingSortTest() {
+    //given when
+    long start = System.currentTimeMillis();
+    CountingSort.sort(array, 150_000);
+    long end = System.currentTimeMillis();
+
+    //then
+    assertThat(array).isEqualTo(compare);
+    loggingExecutionTime(start, end);
+  }
+
   void loggingExecutionTime(long start, long end) {
     String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
     long executionTime = end - start;
