4400e8b87e1dd65bcb6f65d55cd286edc0f967e7
BlanC <kks4517@naver.com>
2024-11-11 15:52:12 +0900

    ✅ Update Problem 041

---
+++ src/main/java/doit/chpater07/problem041/Problem041.java
@@ -0,0 +1,36 @@
+package doit.chpater07.problem041;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem041 {
+
+  public static void main(String[] args) {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+      long n = Long.parseLong(br.readLine());
+
+      long result = n;
+      for (long p = 2; p <= Math.sqrt(n); p++) {
+        if (n % p == 0) {
+          while (n % p == 0) {
+            n /= p;
+          }
+          result = result * (p - 1) / p;
+        }
+      }
+
+      if (n > 1) {
+        result = result * (n - 1) / n;
+      }
+
+      bw.write(String.valueOf(result));
+      bw.flush();
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
+  }
+}
