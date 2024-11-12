246e920875e047b8ac3e48ae62ae1a29ccee6ac2
BlanC <74227460+kihong1602@users.noreply.github.com>
2024-11-12 10:33:58 +0900

    ✅ Update Problem 042

---
+++ src/main/java/doit/chpater07/problem042/Problem042.java
@@ -0,0 +1,40 @@
+package doit.chpater07.problem042;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem042 {
+
+  public static void main(String[] args) {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+      int size = Integer.parseInt(br.readLine());
+      StringBuilder sb = new StringBuilder();
+      while (size-- > 0) {
+        String[] inputs = br.readLine().split(" ");
+        int a = Integer.parseInt(inputs[0]);
+        int b = Integer.parseInt(inputs[1]);
+        int result = solution(a, b);
+        sb.append(result).append("\n");
+      }
+      bw.write(sb.toString());
+      bw.flush();
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
+  }
+
+  private static int solution(int a, int b) {
+    return a * b / gcd(a, b);
+  }
+
+  private static int gcd(int a, int b) {
+    if (b == 0) {
+      return a;
+    }
+    return gcd(b, a % b);
+  }
+}
