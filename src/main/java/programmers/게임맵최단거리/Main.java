ad2a038e4b1c1de9f9882fcee9cf67be91734833
BlanC <74227460+kihong1602@users.noreply.github.com>
2025-01-03 18:44:42 +0900

    ♻️ Modified Programmers 15

--- src/main/java/programmers/게임맵최단거리/Main.java
+++ src/main/java/programmers/게임맵최단거리/Main.java
@@ -5,18 +5,17 @@ import java.util.Queue;
 
 public class Main {
 
-  private int[][] visited;
-  private int[] dx = {-1, 1, 0, 0};
-  private int[] dy = {0, 0, -1, 1};
+  private final int[] dx = {-1, 1, 0, 0};
+  private final int[] dy = {0, 0, -1, 1};
 
   public int solution(int[][] maps) {
-    int n = maps.length;
-    int m = maps[0].length;
-    visited = new int[n][m];
-
     Point start = new Point(0, 0);
     bfs(maps, start);
-    return visited[n - 1][m - 1] == 0 ? -1 : visited[n - 1][m - 1];
+
+    int n = maps.length;
+    int m = maps[0].length;
+
+    return maps[n - 1][m - 1] == 1 ? -1 : maps[n - 1][m - 1];
   }
 
   private void bfs(int[][] maps, Point start) {
@@ -24,7 +23,6 @@ public class Main {
     int height = maps[0].length;
     Queue<Point> queue = new LinkedList<>();
     queue.add(start);
-    visited[start.x][start.y] = 1;
 
     while (!queue.isEmpty()) {
       Point now = queue.poll();
@@ -37,10 +35,10 @@ public class Main {
         if (nextX < 0 || nextY < 0 || nextX >= width || nextY >= height) {
           continue;
         }
-        if (maps[nextX][nextY] == 0 || visited[nextX][nextY] != 0) {
+        if (maps[nextX][nextY] != 1) {
           continue;
         }
-        visited[nextX][nextY] = visited[nowX][nowY] + 1;
+        maps[nextX][nextY] = maps[nowX][nowY] + 1;
         queue.add(new Point(nextX, nextY));
       }
     }
